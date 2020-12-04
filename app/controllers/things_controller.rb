738e3830ec5fa9676d6a10c399e6129e5704948c
MServatti <servatti@hotmail.com>
2020-12-04 15:07:43 -0300

    added home and about pages

---
+++ app/controllers/things_controller.rb
@@ -0,0 +1,6 @@
+class ThingsController < ApplicationController
+    def home
+    end
+    def about
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    root 'things#home'
+    #get '/pages/about' => 'pages#show' , slug: 'page_about'
+    get 'about' => 'things#about'
 end
