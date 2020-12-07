26743222bd66b6de5d1ab22c76640a8c331c763c
MServatti <servatti@outlook.com>
2020-12-07 14:04:23 -0300

    Article listing

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
     root 'things#home'
     #get '/pages/about' => 'pages#show' , slug: 'page_about'
     get 'about' => 'things#about'
-    resources :articles, only: [:show]
+    resources :articles, only: [:show, :index]
 end
